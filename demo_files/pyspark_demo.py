2bf1152c7c799f5e0d5e39c209d53bf4b0e82ad3
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2024-12-15 12:27:40 +0530

    Updated demo pyspark application file and gitignore to ignore data directory

--- demo_files/pyspark_demo.py
+++ demo_files/pyspark_demo.py
@@ -12,18 +12,20 @@ spark.version
 # %%
 df = spark.createDataFrame(
     [
-        ("Jedi", "master"),
-        ("Jedi", "master"),
-        ("Jedi", "master"),
-    ]
+        ("jedi", "master", "anakin", ),
+        ("jedi", "master", "yoda", ),
+        ("jedi", "master", "obiwan", ),
+        ("sith", "master", "palpatine", ),
+    ],
+    ["team", "rank", "name"]
 )
 
 df.show()
 
-df.write.format("parquet").save("jedi_table", mode="overwrite")
+df.write.format("parquet").save("data/jedi_table", mode="overwrite")
 
 # %%
-jedi_df = spark.read.parquet("jedi_table")
+jedi_df = spark.read.parquet("data/jedi_table")
 
 jedi_df.show()
 
